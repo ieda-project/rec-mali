b7e404d93a9b100a9a3f8ca6c07a7c699e4f1c16
Roland Venesz <roland.venesz@wopata.com>
2010-05-14 11:24:29 +0200

    Bugfix

--- app/models/illness.rb
+++ app/models/illness.rb
@@ -1,5 +1,5 @@
 class Illness < ActiveRecord::Base
-  has_many :answers, :class => 'IllnessAnswer'
+  has_many :answers, class_name: 'IllnessAnswer'
   has_many :classifications
   has_many :signs
 end
