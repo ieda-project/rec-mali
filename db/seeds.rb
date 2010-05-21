102bd8f34a096fbacfcaa60175d06980f47f3704
Jean-Etienne Durand <jetienne@etimbp-4.local>
2010-05-21 15:20:05 +0200

    Cannot test in dev, so seed in prod

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,22 @@
-if Rails.env.development?
-  User.create first_name: 'Albert', last_name: 'Schweitzer', login: 'albert'
+User.create first_name: 'Albert', last_name: 'Schweitzer', login: 'albert'
 
-  for n in %w(Kiembara Kongoussi Bobo-Dioulasso Niangoloko).map do
-    Village.create name: n
-  end
+for n in %w(Kiembara Kongoussi Bobo-Dioulasso Niangoloko).map do
+  Village.create name: n
+end
 
-  100.times do
-    child = Child.create(
-      first_name: Faker::Name.first_name,
-      last_name:  Faker::Name.last_name,
-      born_on:    Date.parse('1999-08-11')+rand(1500),
-      village_id: rand(4)+1)
+100.times do
+  child = Child.create(
+    first_name: Faker::Name.first_name,
+    last_name:  Faker::Name.last_name,
+    born_on:    Date.parse('1999-08-11')+rand(1500),
+    village_id: rand(4)+1)
 
-    date = Date.parse('2010-05-05')
-    (rand(5)+3).times do
-      Diagnostic.create(
-        child: child,
-        author_id: 1,
-        done_on: date)
-      date += 1
-    end
+  date = Date.parse('2010-05-05')
+  (rand(5)+3).times do
+    Diagnostic.create(
+      child: child,
+      author_id: 1,
+      done_on: date)
+    date += 1
   end
 end
