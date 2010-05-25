e25f07062147224d2a4f3858b7ffb49fa8e4ec71
Roland Venesz <roland.venesz@wopata.com>
2010-05-25 20:10:14 +0200

    UI changes, added Mootools

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,46 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+Element.implement({
+  classes: function() { return this.className.split(/\s+/) },
+  updated: function(name) {
+    if (name) Element.behaviour_builders_named.get(name).apply(this)
+    else Element.behaviour_builders.each(function (fun) { fun.apply(this) }.bind(this))
+    return this
+  }
+})
+$extend(Element, {
+  behaviour_builders: [],
+  behaviour_builders_named: new Hash(),
+  behaviour: function(a, b) {
+    var fun = typeof(a) == 'function' ? a : b
+    this.behaviour_builders.push(fun)
+    if (typeof(a) == 'string') this.behaviour_builders_named.include(a, fun)
+  }
+})
+
+window.addEvent('domready', function() { document.body.updated() })
+
+Element.behaviour(function() {
+  this.getElements('form.new_child input[type=text]').addEvent('focus', function() {
+    if (this.value == this.get('data-label')) this.value = ''
+    this.removeClass('blank')
+  }).addEvent('blur', function() {
+    if (this.value == '') {
+      this.value = this.get('data-label')
+      this.addClass('blank')
+    } else {
+      this.removeClass('blank')
+    }
+  }).each(function(i) {
+    i.fireEvent('blur')
+  })
+
+  this.getElements('input[type=checkbox]').each(function (cb) {
+    var button = new Element('button', { type: 'button' }).injectAfter(cb)
+    button.setAttribute('type', 'button')
+    button.cb = cb
+    cb.setStyle('display', 'none')
+  })
+  this.getElements('input[type=checkbox]+button').addEvent('click', function(e) {
+    this.cb.checked = !this.cb.checked
+    return false
+  })
+})
