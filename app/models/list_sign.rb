74f8eebd4c771f51e284e46e1e0ede6a82045050
Roland Venesz <roland.venesz@wopata.com>
2010-05-25 20:11:53 +0200

    Sign "kinds", village select

--- app/models/list_sign.rb
+++ app/models/list_sign.rb
@@ -1,2 +1,5 @@
 class ListSign < Sign
+  def options
+    values.split(';')
+  end
 end
--- app/models/sign.rb
+++ app/models/sign.rb
@@ -1,4 +1,8 @@
 class Sign < ActiveRecord::Base
   belongs_to :illness
   has_many :answers, class_name: 'SignAnswer'
+
+  def kind
+    self.class.name.sub(/Sign\Z/, '').underscore
+  end
 end
