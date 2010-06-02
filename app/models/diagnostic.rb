4bc6530ae5e9f41655469adaa7cbb01c7057ab97
Roland Venesz <roland.venesz@wopata.com>
2010-06-02 14:06:21 +0200

    Clear classifications on change

--- app/models/diagnostic.rb
+++ app/models/diagnostic.rb
@@ -2,14 +2,22 @@ class Diagnostic < ActiveRecord::Base
   include Csps::Exportable
   belongs_to :child
   belongs_to :author, class_name: 'User'
-  has_and_belongs_to_many :classifications
-  has_many :illness_answers
-  has_many :sign_answers, include: :sign do
+  has_and_belongs_to_many :classifications do
+    def for illness
+      select { |c| c.illness_id == illness.id }
+    end
+  end
+  has_many :sign_answers, include: :sign,
+           after_add: :clear_classifications, after_remove: :clear_classifications do
     def add data
       sign = data.delete(:sign) || Sign.find(data.delete(:sign_id)) rescue nil
       existing = detect { |i| i.sign_id == sign.id }
       if existing
-        existing.update_attributes data
+        existing.attributes = data
+        if existing.changed?
+          existing.save
+          proxy_owner.send :clear_classifications
+        end
       else
         returning(sign ? sign.build_answer(data) : SignAnswer.new(data)) { |sa| push sa }
       end
@@ -25,6 +33,8 @@ class Diagnostic < ActiveRecord::Base
       end
     end
   end
+  has_many :illness_answers
+
   before_create :set_date
   after_create :update_child
 
@@ -50,5 +60,9 @@ class Diagnostic < ActiveRecord::Base
 
   def set_date
     self.done_on ||= Date.today
+  end
+
+  def clear_classifications
+    classifications.clear
   end
 end
