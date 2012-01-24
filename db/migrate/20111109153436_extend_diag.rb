17d1e1d1eaa4ea018a45ab6ecf77733025bac8a7
Roland Venesz <roland.venesz@wopata.com>
2012-01-24 11:59:31 +0100

    #19026

--- db/migrate/20111109153436_extend_diag.rb
+++ db/migrate/20111109153436_extend_diag.rb
@@ -10,6 +10,20 @@ class ExtendDiag < ActiveRecord::Migration
     Diagnostic.includes(:child).find_each do |diag|
       diag.update_attribute :born_on, diag.child.born_on
     end
+
+    sign = Sign.where(
+      key: 'fievre_presence',
+      age_group: Csps::Age['child']).first.id
+    subselect = SignAnswer.
+      select(:diagnostic_global_id).
+      where(sign_id: sign, boolean_value: true)
+
+    Sign.connection.execute(
+      "UPDATE diagnostics SET temperature=38
+       WHERE global_id IN (#{subselect.to_sql})")
+    Sign.connection.execute(
+      'UPDATE diagnostics SET temperature=37.5
+       WHERE temperature IS NULL')
   end
 
   def self.down
--- db/migrate/20111201140118_rerun_seeding_if_needed.rb
+++ db/migrate/20111201140118_rerun_seeding_if_needed.rb
@@ -1,6 +1,7 @@
 class RerunSeedingIfNeeded < ActiveRecord::Migration
   def self.up
     if Illness.any?
+      ActiveRecord::Base.subclasses.each &:reset_column_information
       load "#{File.dirname(__FILE__)}/../seeds.rb"
     end
   end
