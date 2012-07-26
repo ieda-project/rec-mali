3226a47c3cfb720d3ec9aadf8d386648e72902fc
Roland Venesz <roland.venesz@wopata.com>
2012-07-26 20:15:35 +0200

    Schemafile updated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120724104128) do
+ActiveRecord::Schema.define(:version => 20120726101312) do
 
   create_table "children", :force => true do |t|
     t.integer  "village_id"
