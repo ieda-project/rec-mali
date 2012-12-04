4cee52fa896f13e2130c323b17fd01b8cb87df5f
Roland Venesz <roland.venesz@wopata.com>
2012-12-04 11:59:55 +0100

    Migration fix

--- db/migrate/20120220163643_add_zones_if_needed.rb
+++ db/migrate/20120220163643_add_zones_if_needed.rb
@@ -1,6 +1,6 @@
 class AddZonesIfNeeded < ActiveRecord::Migration
   def self.up
-    if Zone.any? && !Zone.where(name: 'Kaya').any?
+    if Zone.count > 0 && !Zone.where(name: 'Kaya').any?
       ActiveRecord::Base.subclasses.each &:reset_column_information
       load "#{File.dirname(__FILE__)}/../seeds.rb"
     end
